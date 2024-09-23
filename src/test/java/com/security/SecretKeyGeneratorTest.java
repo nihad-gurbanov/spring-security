19b6471dd903547d4423efdaa381466e729e5110
gurbanovnihad <7404@holbertonstudents.com>
2024-09-23 11:36:17 +0400

    jwt secret key generation

---
+++ src/test/java/com/security/SecretKeyGeneratorTest.java
@@ -0,0 +1,18 @@
+package com.security;
+
+import io.jsonwebtoken.Jwts;
+import jakarta.xml.bind.DatatypeConverter;
+import org.junit.jupiter.api.Test;
+
+import javax.crypto.SecretKey;
+
+public class SecretKeyGeneratorTest {
+
+    @Test
+    public void generateSecretKey() {
+        SecretKey key = Jwts.SIG.HS512.key().build();
+        String encodedKey = DatatypeConverter.printHexBinary(key.getEncoded());
+
+        System.out.printf("\n Key = [%s]\n",encodedKey);
+    }
+}
